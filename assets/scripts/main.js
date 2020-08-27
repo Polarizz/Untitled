368d77201223a5b2e062a777c9787962f1c7d5f4
Polarizz <polarizz39@gmail.com>
2020-08-26 17:49:30 -0700

    Finished base, prepare for save button

---
+++ assets/scripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $("#button").click(function(e){
+      $("#card").clone().appendTo("main").find("input,textarea").val("");
+
+    });
+  
+});
+
+function saveTextAsFile(){
+    var textToSave = document.getElementById("name").value;
+    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    var fileNameToSaveAs = document.getElementById("content").value;
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+ 
+    downloadLink.click();
+}
+
