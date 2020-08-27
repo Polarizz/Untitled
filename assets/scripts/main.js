857de8ae0a1dfbb8cb5f8ecdaf501c64d392de6f
Polarizz <polarizz39@gmail.com>
2020-08-26 19:58:51 -0700

    Pseudo code?

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
     });
   
 });
+
+// function saveTextAsFile(){
+//     var textToSave = document.getElementById("name").value;
+//     var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+//     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+//     var fileNameToSaveAs = document.getElementById("content").value;
+
+//     var downloadLink = document.createElement("a");
+//     downloadLink.download = fileNameToSaveAs;
+//     downloadLink.innerHTML = "Download File";
+//     downloadLink.href = textToSaveAsURL;
+//     downloadLink.onclick = destroyClickedElement;
+//     downloadLink.style.display = "none";
+//     document.body.appendChild(downloadLink);
+ 
+//     downloadLink.click();
+// }
+
